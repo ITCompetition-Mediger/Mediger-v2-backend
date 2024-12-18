afe126c62d7830c7259679e1e571371dd92f2ee7
dd-jiyun <dlwldbs8015@gmail.com>
2024-12-19 00:47:08 +0900

    Initial settings

---
+++ src/main/java/net/mediger/MedigerApplication.java
@@ -0,0 +1,13 @@
+package net.mediger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedigerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedigerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mediger/MedigerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mediger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedigerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
