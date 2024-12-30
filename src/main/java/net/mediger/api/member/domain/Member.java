84c07a4a13c7b85c351763b5a795fcef0cb208ab
dd-jiyun <dlwldbs8015@gmail.com>
2024-12-30 23:15:38 +0900

    [#8] feat: 회원 엔티티 추가

---
+++ src/main/java/net/mediger/api/member/domain/Member.java
@@ -0,0 +1,45 @@
+package net.mediger.api.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String account;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Builder
+    public Member(String account, String password, String name, int age) {
+        this.account = account;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
