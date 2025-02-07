39f5e46d027ff1d2edb7eb95eb23fb6935402472
dd-jiyun <dlwldbs8015@gmail.com>
2025-02-07 19:56:45 +0900

    [#16] fix: Swagger 어노테이션 추가

---
+++ src/main/java/net/mediger/member/api/docs/MemberApiDocs.java
@@ -0,0 +1,27 @@
+package net.mediger.member.api.docs;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.security.Principal;
+import net.mediger.global.exception.response.ApiResponse;
+import net.mediger.member.api.dto.RequestBusinessDetails;
+import net.mediger.member.api.dto.RequestDetails;
+
+@Tag(name = "Member Controller", description = "회원 관련 API")
+public interface MemberApiDocs {
+
+    @Operation(summary = "회원 정보 업데이트")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "회원 정보 업데이트 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "회원 정보 업데이트 실패")}
+    )
+    ApiResponse<Void> update(Principal principal, Long id, RequestDetails requestDetails);
+
+    @Operation(summary = "사업자 정보 업데이트")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "사업자 정보 업데이트 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "사업자 정보 업데이트 실패")}
+    )
+    ApiResponse<Void> updateBusiness(Principal principal, Long id, RequestBusinessDetails requestBusinessDetails);
+}
